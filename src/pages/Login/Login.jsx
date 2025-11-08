dcc6da88a093ef98fdab6a6089915d6f6c90309f
priyanshutripathi03 <tripathipriyanshunath@gmail.com>
2025-11-08 15:46:11 +0530

    email or phone to Email only

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
       <div className="login-container">
         <h1 className="login-title">Login</h1>
 
-        <input type="text" className="login-input" placeholder="Email or Phone" />
+        <input type="text" className="login-input" placeholder="Email" />
         <input type="password" className="login-input" placeholder="enter password" />
 
         <button className="login-btn">Login</button>
